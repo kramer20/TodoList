e4807ae97fd1e11a8fb7df782da7c6a70f527658
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-15 13:39:21 -0400

    buttons active

--- js/main.js
+++ js/main.js
@@ -20,7 +20,21 @@ $(document).ready(function() {
 		var newTodo = new ToDoItem(content,false);
 		todo.push(newTodo);
 		toNewBox();
-	})
+	});
+
+	//when click, shows all active
+	$("body").on("click",".show-active",function() {
+		toActive();
+	});
+
+	//when click, shows all completed
+	$("body").on("click",".show-completed",function() {
+		toComplete();
+	});
+
+	$("body").on("click",".show-all",function() {
+		toNewBox();
+	});
 
 	//function for "toNewBox", creates path for each item to run through the array
 	function toNewBox() {
@@ -34,17 +48,45 @@ $(document).ready(function() {
 			else {
 				itemBox.append("<li><article class='completed'><button class='check'></button><p>"+todo.content+"</p><input type='text' class='edit-todo' value='learn css'><button class='delete'>X</button></article></li>");
 			}
-		})
-	}
+		});
+	};
+
+	//function for "active box"
+	function toActive() {
+		var itemBox = $(".items");
+		itemBox.html("");
+
+		todo.forEach(function(todo){
+			if (todo.complete===false){
+				itemBox.append("<li><article><button class='check'></button><p>"+todo.content+"</p><input type='text' class='edit-todo' value='learn html'><button class='delete'>X</button></article></li>");
+			}
+	});
+
+};
+
+	//function for "complete box"
+	function toComplete() {
+		var itemBox = $(".items");
+		itemBox.html("");
+
+		todo.forEach(function(todo){
+			if (todo.complete===true){
+				itemBox.append("<li><article class='completed'><button class='check'></button><p>"+todo.content+"</p><input type='text' class='edit-todo' value='learn css'><button class='delete'>X</button></article></li>");
+			}
+	});
+};
 
 		//function for checking off/striking out
 	$("body").on("click",".check",function() {
-		var content = $(this).parent().find("p").text();
+		var unCheck = $(this).parent().find("p").text();
 		todo.forEach(function(todo){
-			if(content === todo.content){
+			if(todo.content === unCheck && todo.complete === false){
 				todo.complete = true;
 			}
-		})
+			else if(todo.content === unCheck && todo.complete === true){
+				todo.complete = false;
+			}
+		});
 		toNewBox();
 	});
 
@@ -55,11 +97,9 @@ $(document).ready(function() {
 			if(content === aTodo.content){
 				todo.splice(index,1);
 			}
-		})
+		});
 		toNewBox();
 	});
-
-	todo.forEach(function())
 
 });
 
